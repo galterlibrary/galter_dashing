340c4620a7015c46ce8138d4e4627dd982b832a0
Jeremy Prevost <jerrocks@gmail.com>
2014-08-18 15:48:25 -0500

    Dynamically loaded upcoming classes
    - ENV['COURSE_URI'] should return JSON formatted courses
    - closes #1

--- jobs/galter_classes.rb
+++ jobs/galter_classes.rb
@@ -1,9 +1,22 @@
-SCHEDULER.every '15m', :first_in => 0 do
-  # example data: we'll need to pull from our app eventually.
-  classes = [
-    { "label" => "Endnote", "value" => "Thursday, August 21, 2014 12:00 PM" },
-    { "label" => "PubMed", "value" => "Friday, August 22, 2014 12:00 PM" }
-  ]
+require 'net/http'
+require 'time'
+require 'json'
+
+SCHEDULER.every '30m', :first_in => 0 do
+  classes_uri = URI(ENV['COURSE_URI'])
+  response = Net::HTTP.get_response(classes_uri)
+  classes_json = JSON.parse(response.body)
+  classes = []
+
+  classes_json.each do |c|
+    course_date = Time.parse(c['class_date']).strftime('%A, %B %e, %Y %l:%M %p')
+    classes << { 'label' => c['course']['title'], 'value' => course_date }
+  end
+
+  if classes.count == 0
+    classes << { 'label' => 'No upcoming classes scheduled at this time',
+      'value' => '' }
+  end
 
   send_event('galter-classes', { items: classes })
 end
