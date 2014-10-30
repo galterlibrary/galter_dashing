f673ca58c9068c9029b67325428896aa09b5a68a
Jeremy Prevost <jerrocks@gmail.com>
2014-10-30 10:15:44 -0500

    fix time bug

--- jobs/galter_classes.rb
+++ jobs/galter_classes.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'time'
 require 'json'
+require 'active_support/all'
 
 SCHEDULER.every '30m', :first_in => 0 do
   classes_uri = URI(ENV['COURSE_URI'])
@@ -9,10 +10,9 @@ SCHEDULER.every '30m', :first_in => 0 do
   classes = []
 
   classes_json.each do |c|
-    course_date = Time.parse(c['class_date']).
-      strftime('%A, %m/%d/%Y')
-    course_time = Time.parse(c['class_date']).
-      strftime('l:%M %p')
+    Time.zone = 'Central Time (US & Canada)'
+    course_date = Time.zone.parse(c['class_date']).strftime('%A, %m/%d/%Y')
+    course_time = Time.zone.parse(c['class_date']).strftime('%l:%M %p')
     classes << { 'class' => c['course']['title'], 'date' => course_date,
       'time' => course_time }
   end
